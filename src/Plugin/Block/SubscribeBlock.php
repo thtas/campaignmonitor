1a8a978d529d0e548ea6c00ab04a3a9f5bc74cbb
Jeremy <jeremy.l@myplanetdigital.com>
2016-02-23 09:32:06 -0500

    Update the block to current D8 standard

--- src/Plugin/Block/SubscribeBlock.php
+++ src/Plugin/Block/SubscribeBlock.php
@@ -1,8 +1,17 @@
 <?php
+
+/**
+ * @file
+ * Contains \Drupal\campaignmonitor\Plugin\Block\SubscribeBlock.
+ */
+
 namespace Drupal\campaignmonitor\Plugin\Block;
 
 use Drupal\block\BlockBase;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\campaignmonitor\CampaignMonitor;
+
 /**
  * Provides a 'Subscribe' block.
  *
@@ -11,19 +20,20 @@ use Drupal\campaignmonitor\CampaignMonitor;
  *   admin_label = @Translation("Subscribe Block"),
  * )
  */
-class SubscribeBlock extends BlockBase {
+class SubscribeBlock extends BlockBase implements BlockPluginInterface {
+
   /**
    * {@inheritdoc}
    */
-  public function blockForm($form, &$form_state) {
+  public function blockForm($form, FormStateInterface $form_state) {
     $config = $this->getConfiguration();    
-    
     $cm = CampaignMonitor::getConnector();
     $lists = array();
+
     foreach($cm->getLists() as $list_id => $info) {
       $lists[$list_id] = $info['name'];
     }
-    
+
     $form['prefix'] = array(
       '#title' => 'Prefix text for the subscribe block',
       '#type' => 'textarea',
@@ -41,35 +51,36 @@ class SubscribeBlock extends BlockBase {
   /**
    * {@inheritdoc}
    */
-  public function blockSubmit($form, &$form_state) {    
+  public function blockSubmit($form, FormStateInterface $form_state) {
     // Save our custom settings when the form is submitted.
     $this->setConfigurationValue('list_status', $form_state['values']['list_status']);
     $this->setConfigurationValue('prefix', $form_state['values']['prefix']);
   }
-    
+
+  /**
+   * {@inheritdoc}
+   */
   public function build() {
-    $config = $this->getConfiguration();
-    
-        
+    $config = $this->getConfiguration();   
     $list_status = isset($config['list_status'])?$config['list_status']:array();            
     $prefix = isset($config['prefix'])?$config['prefix']:array();            
-    
     $cm = CampaignMonitor::getConnector();
     $lists = $cm->getLists();
-    
+
     $enabled_lists = array();
     foreach($list_status as $list_id => $enabled) {
       if($enabled && isset($lists[$list_id])) {
         $enabled_lists[$list_id] = $lists[$list_id]['name'];
       }
     }    
-    
-    $form = \Drupal::formBuilder()->getForm('Drupal\campaignmonitor\Form\SubscribeForm', array('enabled_lists' => $enabled_lists) );
+
+    $form = \Drupal::formBuilder()->getForm('Drupal\campaignmonitor\Form\SubscribeForm',
+      array('enabled_lists' => $enabled_lists));
+
     return array(
       'prefix' => array('#markup' => $prefix),
       'subscribe_form' => $form,
     );
-    
   }
 }
 ?>
